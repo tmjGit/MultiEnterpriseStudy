30d1dd998268ece690d357d9801707ef5bcf8855
Tobias <git@tmj.li>
2019-07-25 00:24:59 +0200

    cleaned up

--- Jee8Jsf23X/src/main/java/li/tmj/multienterprise/ConfigurationBean.java
+++ Jee8Jsf23X/src/main/java/li/tmj/multienterprise/ConfigurationBean.java
@@ -2,6 +2,10 @@ package li.tmj.multienterprise;
 
 import javax.faces.annotation.FacesConfig;
 
-//Needed for CDI with this JSF-Version. When useing bean-discovery-mode="annotated", it's needed on every Bean!
+/** 
+ * Needed for CDI with this JSF-Version.
+ * When useing bean-discovery-mode="annotated", it's needed on every Bean!
+ * This also activates JSF, so with this, a faces.config.xml is not needed.
+ */
 @FacesConfig(version = FacesConfig.Version.JSF_2_3)
 public class ConfigurationBean { }
